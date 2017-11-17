f2bdec7363522ed94a2d3ec7cdfd70335d471e86
Simon Spannagel <simon.spannagel@cern.ch>
2017-11-17 11:06:23 +0100

    CI: update dependencies

--- .gitlab-ci.d/init_x86_64.sh
+++ .gitlab-ci.d/init_x86_64.sh
@@ -58,10 +58,10 @@ BUILD_FLAVOUR=x86_64-${OS}-${COMPILER_VERSION}-${BUILD_TYPE}
 #--------------------------------------------------------------------------------
 
 if [ ${COMPILER_TYPE} == "gcc" ]; then
-    source ${CLICREPO}/compilers/gcc/7.1.0/x86_64-${OS}/setup.sh
+    source ${CLICREPO}/compilers/gcc/7.2.0/x86_64-${OS}/setup.sh
 fi
 if [ ${COMPILER_TYPE} == "llvm" ]; then
-    source ${CLICREPO}/compilers/llvm/4.0.0/x86_64-${OS}/setup.sh
+    source ${CLICREPO}/compilers/llvm/5.0.0/x86_64-${OS}/setup.sh
 fi
 
 #--------------------------------------------------------------------------------
@@ -75,14 +75,14 @@ export LD_LIBRARY_PATH=${PYTHONDIR}/lib:${LD_LIBRARY_PATH}
 #     CMake
 #--------------------------------------------------------------------------------
 
-export CMAKE_HOME=${CLICREPO}/software/CMake/3.8.1/${BUILD_FLAVOUR}
+export CMAKE_HOME=${CLICREPO}/software/CMake/3.9.5/${BUILD_FLAVOUR}
 export PATH=${CMAKE_HOME}/bin:$PATH
 
 #--------------------------------------------------------------------------------
 #     ROOT
 #--------------------------------------------------------------------------------
 
-export ROOTSYS=${CLICREPO}/software/ROOT/6.08.06/${BUILD_FLAVOUR}
+export ROOTSYS=${CLICREPO}/software/ROOT/6.10.08/${BUILD_FLAVOUR}
 export PYTHONPATH="$ROOTSYS/lib:$PYTHONPATH"
 export PATH="$ROOTSYS/bin:$PATH"
 export LD_LIBRARY_PATH="$ROOTSYS/lib:$LD_LIBRARY_PATH"
@@ -100,7 +100,7 @@ export LD_LIBRARY_PATH="$XercesC_HOME/lib:$LD_LIBRARY_PATH"
 #     Geant4
 #--------------------------------------------------------------------------------
 
-export G4INSTALL=${CLICREPO}/software/Geant4/10.03.p01/${BUILD_FLAVOUR}
+export G4INSTALL=${CLICREPO}/software/Geant4/10.03.p03/${BUILD_FLAVOUR}
 export G4LIB=$G4INSTALL/lib64/Geant4-10.3.1/
 export G4ENV_INIT="${G4INSTALL}/bin/geant4.sh"
 export G4SYSTEM="Linux-g++"
@@ -123,14 +123,14 @@ export LD_LIBRARY_PATH="${BOOST_ROOT}/lib:$LD_LIBRARY_PATH"
 #     Ninja
 #--------------------------------------------------------------------------------
 
-export Ninja_HOME=${CLICREPO}/software/Ninja/1.7.2/${BUILD_FLAVOUR}
+export Ninja_HOME=${CLICREPO}/software/Ninja/1.8.2/${BUILD_FLAVOUR}
 export PATH="$Ninja_HOME:$PATH"
 
 #--------------------------------------------------------------------------------
 #     Eigen
 #--------------------------------------------------------------------------------
 
-export Eigen_HOME=${CLICREPO}/software/Eigen/3.3.3/${BUILD_FLAVOUR}/
+export Eigen_HOME=${CLICREPO}/software/Eigen/3.3.4/${BUILD_FLAVOUR}/
 export Eigen3_DIR=${Eigen_HOME}/share/eigen3/cmake/
 export CMAKE_PREFIX_PATH="$Eigen3_DIR:$CMAKE_PREFIX_PATH"
 
@@ -152,6 +152,5 @@ export PATH=${Git_HOME}/bin:${PATH}
 #     LCIO
 #--------------------------------------------------------------------------------
 
-export LCIO=${CLICREPO}/software/LCIO/2.8.0/${BUILD_FLAVOUR}/
+export LCIO=${CLICREPO}/software/LCIO/2.11.0/${BUILD_FLAVOUR}/
 export CMAKE_PREFIX_PATH="$LCIO:$CMAKE_PREFIX_PATH"
-
