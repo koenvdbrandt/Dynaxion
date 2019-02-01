df056b101c9b79e45db3d7b8e8440fdde6a2e46c
Simon Spannagel <simon.spannagel@cern.ch>
2019-02-01 14:12:16 +0100

    Try LCG_94

--- etc/scripts/setup_lxplus.sh
+++ etc/scripts/setup_lxplus.sh
@@ -10,17 +10,5 @@ ABSOLUTE_PATH=`dirname $(readlink -f ${BASH_SOURCE[0]})`
 source $ABSOLUTE_PATH/../../.gitlab/ci/init_x86_64.sh
 
 # Load different Geant for the moment, because CLICdp version does not have QT
-SFTREPO=/cvmfs/sft.cern.ch/lcg/releases
-
-# FIXME: This should not be a fixed directory
-if [ ${BUILD_FLAVOUR} == "x86_64-slc6-gcc7-opt" ]; then
-    export G4INSTALL=$SFTREPO/Geant4/10.04.p02-0be30/x86_64-slc6-gcc7-opt/
-elif [ ${BUILD_FLAVOUR} == "x86_64-centos7-gcc7-opt" ]; then
-    export G4INSTALL=$SFTREPO/Geant4/10.04.p02-09680/x86_64-centos7-gcc7-opt/
-fi
-
-source $G4INSTALL/bin/geant4.sh
-export CMAKE_PREFIX_PATH="$G4INSTALL:$CMAKE_PREFIX_PATH"
-
-# Point to corresponding CLHEP
-export CMAKE_PREFIX_PATH="$SFTREPO/clhep/2.4.1.0-2c56f/$BUILD_FLAVOUR:$CMAKE_PREFIX_PATH"
+SFTREPO=/cvmfs/sft.cern.ch/lcg/views/LCG_94
+source ${SFTREPO}/${BUILD_FLAVOUR}/setup.sh
